38467487e6914bae8a6aa39e08bdbd6a91c8f3ad
KamenDamov <kamendamov123@gmail.com>
2022-10-09 15:13:48 -0400

    push

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,9 +14,12 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("Streamline NLP")),
-      body: Center(child: Container(color: Colors.red,
+      body: Center(child: Container(
+      color: Colors.red,
       width: 100,
-      height: 100,),),
+      height: 100,
+      child: Text("I'm a box"),
+      ),),
     );
   }
 }
